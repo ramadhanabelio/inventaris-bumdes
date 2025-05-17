7068d05c8186619ee1007b708a76e19525356e5d
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2025-05-17 15:43:41 +0700

    style: image profile path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BUMDes Sumber Rezeki - Desa Bantan Sari</title>
     <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
-    <link rel="icon" href="../assets/img/logo.png" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="image/x-icon" />
     <!-- Fonts and icons -->
     <script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
     <script>
@@ -43,7 +43,8 @@
             <!-- Logo Header -->
             <div class="logo-header" data-background-color="blue">
                 <a href="" class="logo">
-                    <img src="../assets/img/logo.png" alt="navbar brand" class="navbar-brand" width="23%" /> &nbsp;
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="navbar brand" class="navbar-brand"
+                        width="23%" /> &nbsp;
                     <span class="text-white">Sumber
                         Rezeki</span>
                 </a>
@@ -79,7 +80,7 @@
                             <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"
                                 aria-expanded="false">
                                 <div class="avatar-sm">
-                                    <img src="../assets/img/profile.png" alt="..."
+                                    <img src="{{ asset('assets/img/profile.png') }}" alt="..."
                                         class="avatar-img rounded-circle" />
                                 </div>
                             </a>
@@ -88,7 +89,7 @@
                                     <li>
                                         <div class="user-box">
                                             <div class="avatar-lg">
-                                                <img src="../assets/img/profile.png" alt="image profile"
+                                                <img src="{{ asset('assets/img/profile.png') }}" alt="image profile"
                                                     class="avatar-img rounded" />
                                             </div>
                                             <div class="u-text">
@@ -127,7 +128,8 @@
                 <div class="sidebar-content">
                     <div class="user">
                         <div class="avatar-sm float-left mr-2">
-                            <img src="../assets/img/profile.png" alt="..." class="avatar-img rounded-circle" />
+                            <img src="{{ asset('assets/img/profile.png') }}" alt="..."
+                                class="avatar-img rounded-circle" />
                         </div>
                         <div class="info">
                             <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
@@ -257,7 +259,11 @@
                         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                             <div>
                                 <h2 class="text-white pb-2 fw-bold">
-                                    <a href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                    @if (Auth::user()->role === 'admin')
+                                        <a href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                    @else
+                                        <a href="{{ url('home') }}">Dashboard</a>
+                                    @endif
                                 </h2>
                                 <h5 class="text-white op-7 mb-2">
                                     BUMDes Sumber Rezeki - Desa Bantan Sari
@@ -279,78 +285,6 @@
                 </div>
             </footer>
         </div>
-
-        <!-- Custom template | don't include it in your project! -->
-        {{-- <div class="custom-template">
-            <div class="title">Settings</div>
-            <div class="custom-content">
-                <div class="switcher">
-                    <div class="switch-block">
-                        <h4>Logo Header</h4>
-                        <div class="btnSwitch">
-                            <button type="button" class="changeLogoHeaderColor" data-color="dark"></button>
-                            <button type="button" class="selected changeLogoHeaderColor" data-color="blue"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="purple"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="light-blue"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="green"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="orange"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="red"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="white"></button>
-                            <br />
-                            <button type="button" class="changeLogoHeaderColor" data-color="dark2"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="blue2"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="purple2"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="light-blue2"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="green2"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="orange2"></button>
-                            <button type="button" class="changeLogoHeaderColor" data-color="red2"></button>
-                        </div>
-                    </div>
-                    <div class="switch-block">
-                        <h4>Navbar Header</h4>
-                        <div class="btnSwitch">
-                            <button type="button" class="changeTopBarColor" data-color="dark"></button>
-                            <button type="button" class="changeTopBarColor" data-color="blue"></button>
-                            <button type="button" class="changeTopBarColor" data-color="purple"></button>
-                            <button type="button" class="changeTopBarColor" data-color="light-blue"></button>
-                            <button type="button" class="changeTopBarColor" data-color="green"></button>
-                            <button type="button" class="changeTopBarColor" data-color="orange"></button>
-                            <button type="button" class="changeTopBarColor" data-color="red"></button>
-                            <button type="button" class="changeTopBarColor" data-color="white"></button>
-                            <br />
-                            <button type="button" class="changeTopBarColor" data-color="dark2"></button>
-                            <button type="button" class="selected changeTopBarColor" data-color="blue2"></button>
-                            <button type="button" class="changeTopBarColor" data-color="purple2"></button>
-                            <button type="button" class="changeTopBarColor" data-color="light-blue2"></button>
-                            <button type="button" class="changeTopBarColor" data-color="green2"></button>
-                            <button type="button" class="changeTopBarColor" data-color="orange2"></button>
-                            <button type="button" class="changeTopBarColor" data-color="red2"></button>
-                        </div>
-                    </div>
-                    <div class="switch-block">
-                        <h4>Sidebar</h4>
-                        <div class="btnSwitch">
-                            <button type="button" class="selected changeSideBarColor" data-color="white"></button>
-                            <button type="button" class="changeSideBarColor" data-color="dark"></button>
-                            <button type="button" class="changeSideBarColor" data-color="dark2"></button>
-                        </div>
-                    </div>
-                    <div class="switch-block">
-                        <h4>Background</h4>
-                        <div class="btnSwitch">
-                            <button type="button" class="changeBackgroundColor" data-color="bg2"></button>
-                            <button type="button" class="changeBackgroundColor selected" data-color="bg1"></button>
-                            <button type="button" class="changeBackgroundColor" data-color="bg3"></button>
-                            <button type="button" class="changeBackgroundColor" data-color="dark"></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="custom-toggle">
-                <i class="flaticon-settings"></i>
-            </div>
-        </div> --}}
-        <!-- End Custom template -->
     </div>
 
     <!-- Core JS Files -->
@@ -371,7 +305,7 @@
     <!-- Datatables -->
     <script src="{{ asset('assets/js/plugin/datatables/datatables.min.js') }}"></script>
     <!-- Bootstrap Notify -->
-    {{-- <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script> --}}
+    <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
     <!-- jQuery Vector Maps -->
     <script src="{{ asset('assets/js/plugin/jqvmap/jquery.vmap.min.js') }}"></script>
     <script src="{{ asset('assets/js/plugin/jqvmap/maps/jquery.vmap.world.js') }}"></script>
@@ -379,9 +313,6 @@
     <script src="{{ asset('assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
     <!-- Atlantis JS -->
     <script src="{{ asset('assets/js/atlantis.min.js') }}"></script>
-    {{-- <!-- Atlantis DEMO methods, don't include it in your project! -->
-    <script src="{{ asset('assets/js/setting-demo.js') }}"></script>
-    <script src="{{ asset('assets/js/demo.js') }}"></script> --}}
     <script>
         $(document).ready(function() {
             $("#basic-datatables").DataTable({});
